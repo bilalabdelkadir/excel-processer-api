0b28d094d8a3fb505491c0e3aa34863cda95004f
bilalabdelkadir <bilalbinabdelkadir>
2023-06-08 11:55:12 +0300

    feat: i finshed setting up middlewares , typescript and basice server

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { Request, Response } from "express";
+import dotenv from "dotenv";
+import logger from "./utils/logger.js";
+import morganMiddleware from "./middleware/morgan.middleware.js";
+
+dotenv.config({path: 'config/config.env'})
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(morganMiddleware);
+
+app.get("/", (req: Request, res: Response) => {
+    logger.info("Checking the API status: Everything is OK");
+    res.status(200).send({
+        status: "UP",
+        message: "The API is up and running!"
+    });
+});
+
+app.listen(port, () => {
+    logger.info(`Server running on port ${port}`);
+}
+);
