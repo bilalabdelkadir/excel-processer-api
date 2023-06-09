0f65e3a1e45fb2660bd3a7e1baea747ad10ffdb6
bilalabdelkadir <bilalbinabdelkadir>
2023-06-09 10:14:42 +0300

    chore: prettier update

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import logger from "./utils/logger.js";
 import morganMiddleware from "./middleware/morgan.middleware.js";
 
-dotenv.config({path: 'config/config.env'})
+dotenv.config({ path: "config/config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,14 +15,13 @@ app.use(express.json());
 app.use(morganMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
-    logger.info("Checking the API status: Everything is OK");
-    res.status(200).send({
-        status: "UP",
-        message: "The API is up and running!"
-    });
+  logger.info("Checking the API status: Everything is OK");
+  res.status(200).send({
+    status: "UP",
+    message: "The API is up and running!",
+  });
 });
 
 app.listen(port, () => {
-    logger.info(`Server running on port ${port}`);
-}
-);
+  logger.info(`Server running on port ${port}`);
+});
